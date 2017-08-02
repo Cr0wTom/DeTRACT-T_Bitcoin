e6a4891f73ea92e97b4a006417b7b31b4b269b43
Cr0w Tom <cr0wsplace1993@gmail.com>
2017-08-02 14:21:53 +0300

    First complete scripts

--- HashMake.py
+++ HashMake.py
@@ -1,12 +1,36 @@
 #!/usr/bin/python
 import hashlib
 import string
+from Crypto.PublicKey import RSA
+
 CN = raw_input("Which is the primary Host Name of your website (Common Name - CN)?") #Common Name
 O = raw_input("Which is the Organizations name?") #Organization
 O.replace(" ", "_") #Replace the spaces of the Organization with underscores
-PK = raw_input("Which is your publick key?") #Public Key of the owner
+ans = raw_input("Do you have a key pair? [Y]es [N]o, default: [Y]")
+if ans == "Y" or ans == "y" or ans == "" or ans == " ":
+    PK = raw_input("Which is your publick key?") #Public Key of the owner
+else:
+    #pip install pycryptodome
+    print "Public/Private key pair creation:" #RSA Public/Private key pair creation with PyCryptodome
+    print "Warning: This is a pseudo-random generation."
+    print "Warning: If you want complete randomness consider other ways of Public/Private key pair generation."
+
+    secret_code = raw_input("Give an unguessable passphrase: ")
+    key = RSA.generate(2048)
+    encrypted_key = key.exportKey(passphrase=secret_code, pkcs=8, protection="scryptAndAES128-CBC")
+
+    file_out = open("rsa_key.bin", "wb") #Save the keys to the rsa_key.bin
+    file_out.write(encrypted_key)
+
+    print key.publickey().exportKey()
+    PK = key.publickey().exportKey()
+
+
+PK.replace("-----BEGIN RSA PUBLIC KEY-----", "") #Remove header
+PK.replace("-----END RSA PUBLIC KEY-----", "") #Remove footer
 FS = CN + ";" + O + ";" + PK + ";" #Final String - concatenation
 m = hashlib.md5()
 m.update(FS)
-print "The string for hashing is %s" % FS
-print m.hexdigest()
+print "\nThe string for hashing is %s" %FS
+print "\nYour Certificate is: ", m.hexdigest()
+print "Run 'python send-OP_RETURN.py <send-address> <send-amount> <certificate>' to send your certificate to the blockchain."
