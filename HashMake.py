abdc7613c9aab2d944d407d8ee16516c0c3d8847
Cr0w Tom <cr0wsplace1993@gmail.com>
2017-08-03 12:17:39 +0300

    Change MD5 to SHA256

--- HashMake.py
+++ HashMake.py
@@ -1,25 +1,3 @@
-# HashMake.py
-#
-# Copyright (c) Cr0w's Place
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-
 #!/usr/bin/python
 import hashlib
 import string
@@ -48,10 +26,10 @@ else:
     PK = key.publickey().exportKey()
 
 
-PK.replace("-----BEGIN RSA PUBLIC KEY-----", "") #Remove header
-PK.replace("-----END RSA PUBLIC KEY-----", "") #Remove footer
+#PK.replace("-----BEGIN RSA PUBLIC KEY-----", "") #Remove header
+#PK.replace("-----END RSA PUBLIC KEY-----", "") #Remove footer
 FS = CN + ";" + O + ";" + PK + ";" #Final String - concatenation
-m = hashlib.md5()
+m = hashlib.sha256()
 m.update(FS)
 print "\nThe string for hashing is %s" %FS
 print "\nYour Certificate is: ", m.hexdigest()
